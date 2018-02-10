5fc69611dd5d6d4941cfec241a00ee4431d77f67
AyakaTsukiyama <s171033@center.wakayama-u.ac.jp>
2018-02-10 16:12:21 +0900

    エラー表示なし

--- src/main/java/jp/co/tis/tiscon3/controller/CardOrderController.java
+++ src/main/java/jp/co/tis/tiscon3/controller/CardOrderController.java
@@ -58,7 +58,7 @@ public class CardOrderController {
      */
     public HttpResponse inputJob(CardOrderForm form) {
         // エラーを出したくないので強制的にエラーを消す.
-        //form.setErrors(null);
+        form.setErrors(null);
 
         //入力内容が不十分であれば，user登録ページに戻る
         if (form.hasErrors()) {
@@ -78,7 +78,7 @@ public class CardOrderController {
      */
     public HttpResponse modifyUser(CardOrderForm form) {
         // エラーを出したくないので強制的にエラーを消す.
-        //form.setErrors(null);
+        form.setErrors(null);
 
         return templateEngine.render("cardOrder/user", "form", form);
     }
